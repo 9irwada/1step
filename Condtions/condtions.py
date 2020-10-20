3ca1faf446eb9c99140f46dd0935edf71b9d2c2b
9irwada <50562252+9irwada@users.noreply.github.com>
2020-10-20 22:12:02 +0300

    new

--- Condtions/condtions.py
+++
@@ -1,6 +0,0 @@
-username  = input('Please insert your usernmae :')
-passowrd  = input('Please insert your passowrd :')
-if username == 'Nabiot' and passowrd == '2424':
-    print ('Hi Baby')
-else:
-    print ('Error')
